5925aacbafc5d6ea27c4fa8d2bd80b0914de8a52
Jonathan Hernandez <jonathanhernandez@Jonathans-MacBook-Pro.local>
2014-11-02 16:32:18 -0800

    Adding models, basic views

---
+++ weatherproj/models.py
@@ -0,0 +1,9 @@
+import uuid
+from django.db import models
+
+# rain amount in inches for a given month in a given location
+class RainDataMonth(models.Model):
+  month = models.IntegerField()
+  rain = models.FloatField()
+  year = models.IntegerField()
+  id = mdoels.UUIDField(primary_key = True, default=uuid.uuid4, editable=False)
--- weatherproj/views.py
+++ weatherproj/views.py
@@ -2,7 +2,21 @@ from django import http
 from django.template import RequestContext, loader
 import urllib2
 import json
-
+from weatherproj.models import RainDataMonth
 
 def home(request):
-  return http.HttpResponse('Weather home test.')
+  allRain = RainDataMonth.objects.all()
+  retStr = 'Weather home test';
+  if allRain:
+    for rain in allRain:
+      retStr += "<br/>" + rain.year + " " + rain.month + " " rain.rain
+  return http.HttpResponse(retStr)
+
+# runs once to fetch the past n years of data for 
+# 94043 and save it to the db
+def initData(request):
+  rain = Rain(month = 11, year = 2013, rain = 0.12)
+  rain.save()
+  r2 = Rain(month = 1, year = 2012, rain = 1.02)
+  r2.save()
+  return http.HttpResponse('Rain data saved.')
