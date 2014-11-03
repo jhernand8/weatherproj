75d39a02178d8ba6d2b30284ec099e2686c2333e
Jonathan Hernandez <jonathanhernandez@Jonathans-MacBook-Pro.local>
2014-11-02 18:58:58 -0800

    added avg, init

--- weatherproj/models.py
+++ weatherproj/models.py
@@ -5,3 +5,9 @@ class MonthRainData(models.Model):
   month = models.IntegerField()
   rain = models.FloatField()
   year = models.IntegerField()
+
+# stores the average amount of rain for a given month
+class AvgRainByMonth(models.Model):
+  month = models.IntegerField(primary_key = True)
+  avg_rain = models.FloatField()
+
--- weatherproj/views.py
+++ weatherproj/views.py
@@ -4,23 +4,40 @@ import urllib2
 import json
 from weatherproj.models import MonthRainData
 from bs4 import BeautifulSoup
+from datetime import date
 
 def home(request):
   allRain = MonthRainData.objects.all()
   retStr = 'Weather home test';
   if allRain:
     for rain in allRain:
-      retStr += "<br/>" + rain.year + " " + rain.month + " " + rain.rain
-  retStr += "parsed:  " + str(getRainAmountForMonth("KNUQ", 1, 2014))
+      retStr += "<br/>" + str(rain.year) + " " + str(rain.month) + " " + str(rain.rain)
+  retStr += "parsed:  " + str(getRainAmountForMonth("KNUQ", 1, 2014, false))
   return http.HttpResponse(retStr)
 
 # runs once to fetch the past n years of data for 
 # 94043 and save it to the db
 def initData(request):
-  rain = MonthRainData(month = 11, year = 2013, rain = 0.12)
-  rain.save()
-  r2 = MonthRainData(month = 1, year = 2012, rain = 1.02)
-  r2.save()
+  allRain = MonthRainData.objects.all();
+  if allRain:
+    for rain in allRain:
+      rain.delete();
+  avgs = AvgRainByMonth.objects.all()
+  for a in avgs:
+    a.delete()
+
+  today = date.today()
+  for year in range(2004, today.year + 1) :
+    for month in range(1, 13):
+      if year == today.year and month > today.month:
+        break
+      rainAmt = getRainAmountForMonth("KNUQ", month, year, false)
+      rainObj = Rain(month = month, year = year, rain = rainAmt)
+      rainObj.save()
+  for month in range(1, 13):
+    rainAvg = getRainAmountForMonth("KNUQ", month, 2013, true)
+    avg = AvgRainByMonth(month = month, avg_rain = rainAvg)
+    avg.save()
   return http.HttpResponse('Rain data saved.')
 
 # returns the url to use to fetch data from 
@@ -31,7 +48,9 @@ def getUrlForMonth(station, month, year):
 
 # Returns a decimal with the amount of rain as a decimal
 # for the given month and year and place.
-def getRainAmountForMonth(station, month, year):
+# if isAvg, returns the average for the month. Otherwise the
+# total for the month
+def getRainAmountForMonth(station, month, year, isAvg):
   urlForData = getUrlForMonth(station, month, year)
   response = urllib2.urlopen(urlForData)
   responseData = response.readlines()
@@ -39,5 +58,7 @@ def getRainAmountForMonth(station, month, year):
   parsed = BeautifulSoup(responsehtml)
   divs = parsed.select('div[class="precip-to-date"] > strong')
   if divs:
+    if isAvg:
+      return float(divs[1].get_text())
     return float(divs[0].get_text())
   return float("0.00")
