237321d1ffd1a96f509ab164b7073352e668f7c6
Jonathan Hernandez <jonathanhernandez@Jonathans-MacBook-Pro.local>
2014-11-03 20:30:06 -0800

    work on totals

--- weatherproj/views.py
+++ weatherproj/views.py
@@ -13,7 +13,7 @@ from django.utils.safestring import mark_safe
 from sets import Set
 
 def home(request):
-  allRain = MonthRainData.objects.order_by('year', 'month')
+  allRain = MonthRainData.objects.order_by('year', 'month').all()
   allAvgs = AvgRainByMonth.objects.order_by('month')
   retStr = 'Weather home test test ';
   years = Set()
@@ -31,20 +31,37 @@ def home(request):
   yearsObj["years"] = list(years)
 
   avgs = getAvgByMonth()
+  totals = getRunningTotalObj(allRain, allAvgs)
   template = loader.get_template('data.html')
-  # from django.utils.safestring import mark_safe
   context = RequestContext(request, {
                            'month_data' : mark_safe(json.dumps(month_data, cls=DjangoJSONEncoder)),
                            'years' : mark_safe(json.dumps(yearsObj, cls=DjangoJSONEncoder)),
-                           'averages' : mark_safe(json.dumps(avgs, cls=DjangoJSONEncoder)) })
+                           'averages' : mark_safe(json.dumps(avgs, cls=DjangoJSONEncoder)),
+                           'totals' : mark_safe(json.dumps(totals, cls=DjangoJSONEncoder))
+                           })
   return http.HttpResponse(template.render(context))
 
 # Constructs object to pass to html for the running total and average
 # running total for each month in each year of rainfall for that season -
 # season being July 1 - June 30.
 def getRunningTotalObj(allRain, allAvgs):
+  runningTotalAvg = getRunningTotalAverages(allAvgs)
   totalObj = {}
+  totalObj["runningTotals"] = runningTotalAvg
+  
   return totalObj;
+
+# Returns object with the average running total rainfall during season thru month.
+def getRunningTotalAverages(allAvgs):
+  totals = {}
+  sum = 0
+  for month in range(7, 13):
+    sum += allAvgs[month - 1].avg_rain
+    totals[month] = sum
+  for month in range(1, 7):
+    sum += allAvgs[month - 1].avg_rain
+    totals[month] = sum
+  return totals
   
 # Returns object of the average rain by month which can be converted to a json object
 def getAvgByMonth():
