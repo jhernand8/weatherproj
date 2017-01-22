f7a762823cfb0f623f78e7745908a353d6644415
Jonathan Hernandez <jbhernan@gmail.com>
2017-01-22 10:46:55 -0800

    Small fix

--- weatherproj/management/commands/updateraindata.py
+++ weatherproj/management/commands/updateraindata.py
@@ -104,7 +104,7 @@ class Command(BaseCommand):
       for month in range(1, 13):
         if self.needsAvgData(currZipToUrl, month, allAvgs):
           rainAvg = self.getRainAmountForMonth(currZipToUrl, month, 2013, True)
-          avg = AvgRainByMonth(month = month, avg_rain = rainAvg, zip = currUrlToZip.zip)
+          avg = AvgRainByMonth(month = month, avg_rain = rainAvg, zip = currZipToUrl.zip)
           avg.save()
           
 
